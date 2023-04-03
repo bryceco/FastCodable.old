403be486d2c57b1cd9cedd748db3688abec3d7fa
Bryce Cogswell <bryceco@yahoo.com>
2023-04-02 19:44:01 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,12 +1,15 @@
-// swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "FastCodable",
+	platforms: [
+		.macOS(.v10_10),
+		.iOS(.v11),
+		.tvOS(.v11),
+		.watchOS(.v4)
+	],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FastCodable",
             targets: ["FastCodable"]),
@@ -16,13 +19,12 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FastCodable",
             dependencies: []),
         .testTarget(
             name: "FastCodableTests",
             dependencies: ["FastCodable"]),
-    ]
+    ],
+	swiftLanguageVersions: [.v5]
 )
